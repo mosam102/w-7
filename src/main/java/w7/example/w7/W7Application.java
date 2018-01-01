a6155da6cb65915ba5342ed13f2dd7b908d78e0e
mo <mahbod_zamani@yahoo.com>
2018-01-01 22:55:46 +0100

    first commit

---
+++ src/main/java/w7/example/w7/W7Application.java
@@ -0,0 +1,12 @@
+package w7.example.w7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class W7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(W7Application.class, args);
+	}
+}
